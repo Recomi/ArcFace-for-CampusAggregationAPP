8b599f6e49a9676457ac26f711353e6954faa6df
Recomi <1281540128@qq.com>
2019-04-10 19:09:31 +0800

    add

--- app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.arcsoft.arcfacedemo.common;
-
-public class Constants {
-    public static final String APP_ID = "6p3hoYFnMNjdiG5Fa7z8YA9JPJppuuZ2TFCGLn1mXT6X";
-    public static final String SDK_KEY = "AHbrVJ11TE9Q1bDZYZmjzQ9qtkEYss4bXij89hwQ455g";
-}
